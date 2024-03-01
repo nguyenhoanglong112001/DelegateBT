8082b2186e7247146e4aaf22161a924c60dd6dbe
DESKTOP-1OGKUDR\Long <nguyenhoanglong112001@gmail.com>
2024-03-01 08:28:00 +0700

    no message

---
+++ BTDelegate/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTDelegate
+{
+    public class Enemy : Chracter
+    {
+        public Enemy(CharacterType Type, int atk, int power, int health, int AtkSpeed) : base(Type, atk, power, health, AtkSpeed)
+        {
+        }
+    }
+}
